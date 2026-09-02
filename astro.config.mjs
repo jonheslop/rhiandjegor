07091891a8d4db3507c8b0acd4bc861aeb64b47d
Jon Heslop <jon@jonheslop.com>
2026-09-02 21:51:37 +0100

    First stab for Rhi

---
+++ astro.config.mjs
@@ -0,0 +1,26 @@
+// @ts-check
+import { defineConfig, fontProviders } from "astro/config";
+
+import tailwindcss from '@tailwindcss/vite';
+
+// https://astro.build/config
+export default defineConfig({
+  fonts: [{
+    provider: fontProviders.fontsource(),
+    name: "Calistoga",
+    cssVariable: "--font-calistoga",
+  },
+  {
+    provider: fontProviders.fontsource(),
+    name: "Abril Fatface",
+    cssVariable: "--font-abrilfatface",
+  },
+  {
+    provider: fontProviders.fontsource(),
+    name: "Vollkorn",
+    cssVariable: "--font-vollkorn",
+  }],
+  vite: {
+    plugins: [tailwindcss()]
+  }
+});
